0187646d77cd318b3cd0f321d39a9e78166438d9
Eduardo Vasconcelos <c.eduvasconcelos@gmail.com>
2023-08-19 03:14:25 -0300

    Implementar rodapé

---
+++ src/components/FooterNavigation.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <v-footer class="d-flex justify-space-between py-4 px-0 mt-4" border>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <div class="d-flex flex-column flex-md-row">
+            <div class="d-flex justify-space-between justify-md-start">
+              <v-btn
+                variant="text"
+                rounded="lg"
+                size="small"
+                :to="{ name: 'Home' }"
+                :active="false"
+              >
+                Início
+              </v-btn>
+
+              <v-btn
+                variant="text"
+                rounded="lg"
+                size="small"
+                class="ms-md-6"
+                :active="false"
+              >
+                Produtos
+              </v-btn>
+            </div>
+
+            <div class="d-flex justify-space-between justify-md-start">
+              <v-btn
+                variant="text"
+                rounded="lg"
+                size="small"
+                class="ms-md-6"
+                :to="{ name: 'Cart' }"
+                :active="false"
+              >
+                Carrinho
+              </v-btn>
+            </div>
+          </div>
+        </v-col>
+
+        <v-col cols="12">
+          <div class="d-flex flex-column flex-md-row justify-space-between align-center">
+            <div class="d-flex flex-column flex-md-row align-center">
+              <div>
+                <v-img
+                  aspect-ratio="16/9"
+                  src="@/assets/logo.jpeg"
+                  width="95px"
+                  height="50px"
+                ></v-img>
+              </div>
+
+              <v-btn
+                class="d-md-none"
+                icon="mdi-instagram"
+                variant="text"
+              ></v-btn>
+
+              <p class="text-caption text-center ms-4 d-none d-md-block">Copyright © {{ new Date().getFullYear() }} Razante 3D. Todos os direitos reservados.</p>
+            </div>
+
+            <v-btn
+              class="mx-4 d-none d-md-block"
+              icon="mdi-instagram"
+              variant="text"
+            ></v-btn>
+
+            <p class="text-caption text-center ms-4 d-md-none">Copyright © {{ new Date().getFullYear() }} Razante 3D. Todos os direitos reservados.</p>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,12 +8,42 @@ const cartStore = useCartStore()
 </script>
 
 <template>
-  <v-container>
+  <v-container fluid>
     <v-row>
+      <v-col cols="12">
+        <v-parallax class="rounded-lg">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+            :height="400"
+            cover
+          ></v-img>
+        </v-parallax>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12">
+        <div class="d-flex align-baseline mb-2">
+          <h1 class="text-h4 text-no-wrap font-weight-black me-2">Mais vendidos</h1>
+
+          <v-icon
+            color="red"
+            icon="mdi-fire-circle"
+            size="large"
+            class="me-4"
+          ></v-icon>
+
+          <v-divider></v-divider>
+        </div>
+
+      </v-col>
+
       <v-col
         v-for="product in appStore.products"
         :key="product.id"
-        cols="12"
+        cols="6"
         sm="4"
         md="3"
       >
@@ -21,6 +51,16 @@ const cartStore = useCartStore()
           :product="product"
           @onBuying="cartStore.add"
         />
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <v-btn
+          variant="outlined"
+          rounded="lg"
+          text="Ver todos os produtos"
+          class="px-16"
+          block
+        ></v-btn>
       </v-col>
     </v-row>
   </v-container>
